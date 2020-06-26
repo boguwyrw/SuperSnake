5b23c1333161efc055cac09b6119c86274098067
boguwyrw <b.wyrwal@wp.pl>
2020-06-26 21:12:05 +0200

    Basic level - done
    Tasks:
    1. Collisions
    2. Snake grow

---
+++ SuperSnake/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    [SerializeField]
+    private Button rightButton;
+    [SerializeField]
+    private Button leftButton;
+
+    private void Awake()
+    {
+        rightButton.transform.position = new Vector3(0.9f * Screen.width, 0.1f * Screen.height, 0.0f);
+        leftButton.transform.position = new Vector3(0.1f * Screen.width, 0.1f * Screen.height, 0.0f);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
---
+++ SuperSnake/Assets/Scripts/SnakeHead.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeHead : MonoBehaviour
+{
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime);
+    }
+
+    public void TurnRight()
+    {
+        transform.Rotate(new Vector3(0, 90, 0));
+    }
+
+    public void TurnLeft()
+    {
+        transform.Rotate(new Vector3(0, -90, 0));
+    }
+
+}
