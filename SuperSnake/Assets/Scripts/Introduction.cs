725a63ff947eb9a86e63f0101281e1c6c64ad066
boguwyrw <b.wyrwal@wp.pl>
2021-05-16 11:33:50 +0200

    Introduction scene has been added

---
+++ SuperSnake/Assets/Scripts/Introduction.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Introduction : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+            SceneManager.LoadScene("MainMenu");
+    }
+}
