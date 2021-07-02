66d50b8a1c9ea1aa31879bb664a91f5f81b91e55
boguwyrw <b.wyrwal@wp.pl>
2021-07-02 10:34:50 +0200

    Sound on off button has been added

---
+++ SuperSnake/Assets/Scripts/SoundOnOff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class SoundOnOff : MonoBehaviour, IPointerDownHandler
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private Sprite soundOn;
+    [SerializeField] private Sprite soundOff;
+
+    private float audioSourceStartVolume;
+    private Image soundImage;
+
+    private void Start()
+    {
+        audioSourceStartVolume = audioSource.volume;
+        soundImage = GetComponent<Image>();
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (audioSource.volume > 0.0f)
+        {
+            audioSource.volume = 0.0f;
+            soundImage.sprite = soundOff;
+        }
+        else
+        {
+            audioSource.volume = audioSourceStartVolume;
+            soundImage.sprite = soundOn;
+        }
+    }
+}
